4660b1b8110a46bff083ac02bd5e7ea2f329bdbe
Григорій . <mcwertyge@gmail.com>
2025-06-09 22:15:05 +0300

    add features(Calculator)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,21 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//  {Console.Write("");
-//  {[var name = Console.ReadLine();
-//  {[{значення2} name = Convert.To{значення2}(Console.ReadLine())
-//Console.WriteLine($"{name}");     $ - Форматування, @ - Сприймати тільки за текст, \n - Перехід на нову строку(писати в "")
-//
-//  Синтаксиз умови:
-//
-//  if (условие)
-//  {
-        // "==" - Дорівнює, "!=" - Не дорівнює, "<"/">" - Меньше/Більше, "<="/">=" - Меньше дорівнює/Більше дорівнює
-//  }
-//  else if (другое_условие)
-//  {
-        // "++" - Збільшення на 1, "--" - Зменьшення на 1, "||" - Або, "&&" - І
-//  }
-//  else
-//  {
-        // 
-//  }
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Write("Яку дію будете використовувати?\n+(Додавання), -(Мінусування), *(Множення), /(Ділення)\n");
+var operation = Console.ReadLine();
+if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+{
+    Console.WriteLine("Помилка: невідома операція.");
+    return;
+}
+
+Console.Write("Введіть першу цифру:\n");
+var firstNumber = Convert.ToInt16(Console.ReadLine());
+
+Console.Write("Введіть другу цифру:\n");
+var secondNumber = Convert.ToInt16(Console.ReadLine());
+
+int result = 0;
+var validOperation = true;
+
+switch (operation)
+{
+        case "+":
+                result = firstNumber + secondNumber;
+                break;
+        case "-":
+                result = firstNumber - secondNumber;
+                break;
+        case "*":
+                result = firstNumber * secondNumber;
+                break;
+        case "/":
+                if (secondNumber == 0)
+                {
+                        Console.WriteLine("На 0 ділити не можна!(Ну взагалі можна... Ну добре, буде тобі результат!)");
+                        Console.WriteLine("Ваш результат: ∞");
+                        validOperation = false;
+                        break;
+                }
+                else
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine($"Ваш результат: {result}");
+                break;
+}
+if (validOperation = true && operation != "/")
+{
+    Console.WriteLine($"Ваш результат: {result}");
+}
